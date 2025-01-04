c3ba6712d3c864b53880b340112803119e37dcd0
Taha Anılcan Metinyurt <igoodie@programmer.net>
2025-01-04 18:56:30 +0300

    feat: update OutfitPiece

--- common/src/main/java/abeshutt/staracademy/outfit/core/OutfitPiece.java
+++ common/src/main/java/abeshutt/staracademy/outfit/core/OutfitPiece.java
@@ -1,17 +1,35 @@
 package abeshutt.staracademy.outfit.core;
 
 import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.model.TexturedModelData;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import net.minecraft.util.Identifier;
 
-public abstract class OutfitPiece extends PlayerEntityModel<AbstractClientPlayerEntity> {
+public abstract class OutfitPiece {
 
-    public OutfitPiece(ModelPart root, boolean thinArms) {
-        super(root, thinArms);
+    protected Identifier id;
+    protected boolean thinArms;
+
+    public OutfitPiece(Identifier id, boolean thinArms) {
+        this.id = id;
+        this.thinArms = thinArms;
     }
 
-    public abstract OutfitSlot getOutfitSlot();
+    public abstract TexturedModelData getTexturedModelData();
 
+    public OutfitPieceModel buildModel() {
+        TexturedModelData texturedModelData = getTexturedModelData();
+        ModelPart root = texturedModelData.createModel();
+        return new OutfitPieceModel(root, this.thinArms);
+    }
 
+    public static class OutfitPieceModel extends PlayerEntityModel<AbstractClientPlayerEntity> {
+
+        public OutfitPieceModel(ModelPart root, boolean thinArms) {
+            super(root, thinArms);
+        }
+
+    }
 
 }
