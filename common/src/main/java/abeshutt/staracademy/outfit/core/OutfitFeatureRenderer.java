7bfb5a6ad663f1b22c8724840a23651107ff2aac
Taha Anılcan Metinyurt <igoodie@programmer.net>
2025-01-04 19:28:42 +0300

    feat: impl rendering logic of Outfits

--- common/src/main/java/abeshutt/staracademy/outfit/core/OutfitFeatureRenderer.java
+++ common/src/main/java/abeshutt/staracademy/outfit/core/OutfitFeatureRenderer.java
@@ -1,5 +1,6 @@
 package abeshutt.staracademy.outfit.core;
 
+import abeshutt.staracademy.StarAcademyMod;
 import abeshutt.staracademy.outfit.models.ClassyOutfit;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.OverlayTexture;
@@ -10,6 +11,7 @@ import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
 
 public class OutfitFeatureRenderer<
         M extends PlayerEntityModel<AbstractClientPlayerEntity>
@@ -24,14 +26,22 @@ public class OutfitFeatureRenderer<
         // TODO: For each equipped outfit pieces
         for (int i = 0; i < 1; i++) {
             // TODO: Get equipped outfit piece models one by one
-            ClassyOutfit.Hat hat = new ClassyOutfit.Hat(null, false);
+            OutfitPiece HAT_PIECE = new ClassyOutfit.Hat(StarAcademyMod.id("classy.hat"), false);
+            OutfitPiece.OutfitPieceModel model = HAT_PIECE.buildModel();
+
 
             // TODO: Fetch texture for current outfit piece model
-            VertexConsumer vertices = vertexConsumers.getBuffer(RenderLayer.getArmorCutoutNoCull(null));
+            String id = "textures/models/armor/" + "gold" + "_layer_"
+                    + 1 // + (secondLayer ? 2 : 1)
+                    + "" // + (overlay == null ? "" : "_" + overlay)
+                    + ".png";
+            VertexConsumer vertices = vertexConsumers.getBuffer(RenderLayer.getArmorCutoutNoCull(
+                    new Identifier(id)
+            ));
 
             // TODO: Copy rotations & render the model
-            this.getContextModel().copyStateTo(hat);
-            hat.render(matrices, vertices, light, OverlayTexture.DEFAULT_UV, 1, 1, 1, 1);
+            this.getContextModel().copyBipedStateTo(model);
+            model.render(matrices, vertices, light, OverlayTexture.DEFAULT_UV, 1, 1, 1, 1);
         }
     }
 
--- common/src/main/java/abeshutt/staracademy/outfit/core/OutfitPiece.java
+++ common/src/main/java/abeshutt/staracademy/outfit/core/OutfitPiece.java
@@ -1,7 +1,6 @@
 package abeshutt.staracademy.outfit.core;
 
-import net.minecraft.client.model.ModelPart;
-import net.minecraft.client.model.TexturedModelData;
+import net.minecraft.client.model.*;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.util.Identifier;
@@ -14,6 +13,37 @@ public abstract class OutfitPiece {
     public OutfitPiece(Identifier id, boolean thinArms) {
         this.id = id;
         this.thinArms = thinArms;
+    }
+
+    protected ModelData getBaseModelData() {
+        ModelData modelData = new ModelData();
+        ModelPartData modelPartData = modelData.getRoot();
+
+        String[] partNames = {
+                "head",
+                "hat",
+                "body",
+                "right_arm",
+                "right_leg",
+                "right_sleeve",
+                "right_pants",
+                "left_arm",
+                "left_leg",
+                "left_sleeve",
+                "left_pants",
+                "ear",
+                "cloak",
+                "jacket",
+        };
+
+        for (String partName : partNames) {
+            modelPartData.addChild(partName,
+                    ModelPartBuilder.create(),
+                    ModelTransform.pivot(0.0F, 0.0F, 0.0F)
+            );
+        }
+
+        return modelData;
     }
 
     public abstract TexturedModelData getTexturedModelData();
