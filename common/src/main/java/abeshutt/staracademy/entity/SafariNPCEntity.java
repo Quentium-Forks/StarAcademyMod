225113f9d5c623aac64b0b39ae26a191342c0c54
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2025-01-04 23:24:17 -0500

    Make safari npc invulnerable to damage

--- common/src/main/java/abeshutt/staracademy/entity/SafariNPCEntity.java
+++ common/src/main/java/abeshutt/staracademy/entity/SafariNPCEntity.java
@@ -4,19 +4,17 @@ import abeshutt.staracademy.StarAcademyMod;
 import abeshutt.staracademy.init.ModWorldData;
 import abeshutt.staracademy.world.data.SafariData;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.goal.LookAroundGoal;
+import net.minecraft.entity.ai.goal.LookAtEntityGoal;
+import net.minecraft.entity.ai.goal.SwimGoal;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Items;
-import net.minecraft.recipe.Ingredient;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
-
-import java.util.UUID;
 
 public class SafariNPCEntity extends HumanEntity {
 
@@ -27,8 +25,14 @@ public class SafariNPCEntity extends HumanEntity {
     @Override
     protected void initGoals() {
         this.goalSelector.add(0, new SwimGoal(this));
-        this.goalSelector.add(1, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.add(1, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F, 0.1F));
         this.goalSelector.add(2, new LookAroundGoal(this));
+    }
+
+    @Override
+    public void tick() {
+        this.setInvulnerable(true);
+        super.tick();
     }
 
     @Override
