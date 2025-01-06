e377fd9c758d850fa20169c7ecd8ea31ad55518c
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2025-01-05 22:05:43 -0500

    Oops compilation issue

--- common/src/main/java/abeshutt/staracademy/world/data/ShootingStarData.java
+++ common/src/main/java/abeshutt/staracademy/world/data/ShootingStarData.java
@@ -2,11 +2,18 @@ package abeshutt.staracademy.world.data;
 
 import net.minecraft.nbt.NbtCompound;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 public class ShootingStarData extends WorldData {
 
-    private Map<>
+    private Map<UUID, ShootingStar> stars;
+
+    public ShootingStarData() {
+        this.stars = new HashMap<>();
+    }
 
     @Override
     public Optional<NbtCompound> writeNbt() {
