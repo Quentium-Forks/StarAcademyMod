9188daa4e2d7b5d09d66256b542b285a0e914b87
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2025-01-08 00:22:43 -0500

    Fix redirect

--- common/src/main/java/abeshutt/staracademy/mixin/cloudboots/MixinGoldenFeatherTrinket.java
+++ common/src/main/java/abeshutt/staracademy/mixin/cloudboots/MixinGoldenFeatherTrinket.java
@@ -7,13 +7,12 @@ import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TrinketsCompat.GoldenFeatherTrinket.class)
 public class MixinGoldenFeatherTrinket {
 
-    @Redirect(method = "lambda$tick$0", at = @At(value = "HEAD"), require = 0)
-    void onBreak(ItemStack stack, SlotReference slot, LivingEntity entity, CallbackInfo ci) {
+    @Redirect(method = "lambda$tick$0", at = @At(value = "INVOKE", target = "Lcom/tiviacz/cloudboots/TrinketsCompat$GoldenFeatherTrinket;onBreak(Lnet/minecraft/item/ItemStack;Ldev/emi/trinkets/api/SlotReference;Lnet/minecraft/entity/LivingEntity;)V"), require = 0)
+    void onBreak(TrinketsCompat.GoldenFeatherTrinket instance, ItemStack stack, SlotReference slotReference, LivingEntity entity) {
         entity.playEquipmentBreakEffects(stack);
     }
 
